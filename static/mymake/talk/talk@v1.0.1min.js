f13bbd2743fad27f81cdcfa7ae2ceca3c7470e57
Blue <530022025@qq.com>
2020-04-22 09:03:40 +0800

    修改时光笔记的js代码

--- static/mymake/talk/talk@v1.0.1min.js
+++ static/mymake/talk/talk@v1.0.1min.js
@@ -20,7 +20,7 @@ function siteTime() {
   minutes - 0-59之间的整数，做为date对象的分钟数
   seconds - 0-59之间的整数，做为date对象的秒数
   microseconds - 0-999之间的整数，做为date对象的毫秒数 */
-  var t1 = Date.UTC(2020, 02, 14, 13, 14, 00);
+  var t1 = Date.UTC(2020, 02, 12, 19, 32, 00);
   var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond);
   var diff = t2 - t1;
   var diffYears = Math.floor(diff / years);
@@ -36,3 +36,7 @@ function siteTime() {
   // + diffYears + " 年 "
 }
 siteTime()
+
+$(document).hover(function() {
+	uploadMusic.play()
+})
